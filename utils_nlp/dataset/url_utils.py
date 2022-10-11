f93ba9a3a04234578e04e79126217d3c6e39fd06
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 08:17:24 +0000

    Adding tarfile member sanitization to extractall()

--- utils_nlp/dataset/url_utils.py
+++ utils_nlp/dataset/url_utils.py
@@ -102,7 +102,26 @@ def extract_tar(file_path, dest_path="."):
     if not os.path.exists(dest_path):
         raise IOError("Destination directory doesn't exist")
     with tarfile.open(file_path) as t:
-        t.extractall(path=dest_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, path=dest_path)
 
 
 def extract_zip(file_path, dest_path="."):
